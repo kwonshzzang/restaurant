24f50829d43db411dd658769a2d522221147a2f9
kwonshzzang <kwonshzzang@gmail.com>
2023-08-25 09:57:16 +0900

    First Commit

---
+++ src/test/java/kr/co/kwonshzzang/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kwonshzzang.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

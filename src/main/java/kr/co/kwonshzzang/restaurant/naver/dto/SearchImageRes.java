5a3cb51366ac9d6c5b663f504ba74247188f66b1
kwonshzzang <kwonshzzang@gmail.com>
2023-09-01 10:21:02 +0900

    Add Naver API function

---
+++ src/main/java/kr/co/kwonshzzang/restaurant/naver/dto/SearchImageRes.java
@@ -0,0 +1,29 @@
+package kr.co.kwonshzzang.restaurant.naver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SearchImageRes {
+    private String lastBuildDate;
+    private int total;
+    private int start;
+    private int display;
+    private List<SearchImageRes.SearchImageItem> items;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public static class SearchImageItem {
+        private String title;
+        private String link;
+        private String thumbnail;
+        private String sizeheight;
+        private String sizewidth;
+    }
+}
---
+++ src/test/java/kr/co/kwonshzzang/restaurant/naver/NaverClientTests.java
@@ -0,0 +1,38 @@
+package kr.co.kwonshzzang.restaurant.naver;
+
+import kr.co.kwonshzzang.restaurant.naver.dto.SearchImageReq;
+import kr.co.kwonshzzang.restaurant.naver.dto.SearchImageRes;
+import kr.co.kwonshzzang.restaurant.naver.dto.SearchLocalReq;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NaverClientTests {
+    @Autowired
+    private NaverClient naverClient;
+
+
+    @Test
+    void seachLocalTest() {
+        var search = new SearchLocalReq();
+        search.setQuery("갈비집");
+
+        var result = naverClient.searchLocal(search);
+
+        System.out.println(result);
+    }
+
+    @Test
+    void searchImageTest() {
+        var search = new SearchImageReq();
+        search.setQuery("갈비집");
+
+        var result = naverClient.searchImage(search);
+
+        System.out.println(result);
+    }
+
+}
